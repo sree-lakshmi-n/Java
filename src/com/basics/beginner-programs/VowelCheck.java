09d788659a4a209684b0b6d0c3e4063ee299e219
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-01-03 16:43:27 +0530

    Added VowelCheck.java

---
+++ src/com/basics/beginner-programs/VowelCheck.java
@@ -0,0 +1,22 @@
+package com.basics.beginnerprograms;
+
+import java.util.Scanner;
+
+public class VowelCheck {
+    private boolean isVowel(char ch){
+        String vowel = "aeiouAEIOU";
+        if(vowel.indexOf(ch)>=0) return true;
+        else return false;
+    }
+    public void display(Object result){
+        System.out.println(result);
+    }
+    public static void main(String[] args) {
+        VowelCheck vw = new VowelCheck();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a character");
+        char ch = sc.next().charAt(0);
+        if(vw.isVowel(ch)) vw.display(ch+" is a vowel");
+        else vw.display(ch+ " is not a vowel");
+    }
+}
