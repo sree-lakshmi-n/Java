6ac5d27ab11ab58ab3850da12f004743a685fba9
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-01-03 18:33:04 +0530

    Created array folder and added ArrayInitialisation.java

---
+++ src/com/basics/array/ArrayInitialisation.java
@@ -0,0 +1,22 @@
+package com.basics.beginnerprograms;
+
+public class ArrayInitialisation {
+    public static void main(String[] args) {
+        int arr1[] = new int[]{1,1,1,1};
+        int[] arr2 = new int[]{2,2,2,2};
+        int []arr3 = {3,3,3,3};
+
+        int []intArr = new int[20];
+
+        char[] charArr;
+        String []strArr;
+        byte byteArr[];
+        short shortArr[];
+        long longArr[];
+        float floatArr[];
+        double doubleArr[];
+        Object objArr[];
+        
+        ArrayInitialisation []classArr;
+    }
+}
