be7cb47bb44bd2b3b776e9bc0d508f3c82467b3c
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-01-03 16:48:23 +0530

    Checks whether given number is prime or not

---
+++ src/com/basics/beginner-programs/PrimeNumber.java
@@ -0,0 +1,30 @@
+// Checks whether given number is prime or not
+
+package com.basics.beginnerprograms;
+
+import java.util.Scanner;
+
+public class PrimeNumber{
+    private boolean isPrime(int num){
+        boolean result = true;
+        for (int i = 2; i < (num/2) + 1; i++) {
+            if(num%i==0){
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PrimeNumber p = new PrimeNumber();
+        System.out.println("Enter a number: ");
+        int num = Math.abs(sc.nextInt());
+        if(num==0||num==1){
+            System.out.println(num+ " is neither prime nor composite.");
+        }
+        else{
+            System.out.println(p.isPrime(num)?num+" is prime":num+" is not prime.");
+        }
+    }
+}
