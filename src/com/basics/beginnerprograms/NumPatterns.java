00de1d971dc0aa9ce0cc7afaeefd117245258eac
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-01-04 10:21:44 +0530

    Number Patterns

--- src/com/basics/beginnerprograms/NumPatterns.java
+++ src/com/basics/beginnerprograms/NumPatterns.java
@@ -496,21 +496,6 @@ public class NumPatterns{
             System.out.println();
         }
     }
-    /*
-    1   2   3   4   5   6   7   8   9   10
-    36  37  38  39  40  41  42  43  44  11
-    35  64  65  66  67  68  69  70  45  12
-    34  63  84  85  86  87  88  71  46  13
-    33  62  83  96  97  98  89  72  47  14
-    32  61  82  95  100 99  90  73  48  15
-    31  60  81  94  93  92  91  74  49  16
-    30  59  80  79  78  77  76  75  50  17
-    29  58  57  56  55  54  53  52  51  18
-    28  27  26  25  24  23  22  21  20  19
-     */
-    public void pattern31(){
-
-    }
     public static void main(String[] args) {
         NumPatterns np = new NumPatterns();
         np.pattern1();
@@ -543,6 +528,5 @@ public class NumPatterns{
         np.pattern28();
         np.pattern29();
         np.pattern30();
-//        np.pattern31();
     }
 }
