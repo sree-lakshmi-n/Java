fdebd56da415a6085f33e20a67a06aae5e388a48
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-01-11 21:02:57 +0530

    StringBuilder demo

---
+++ src/com/basics/Strings/StringBldr.java
@@ -0,0 +1,11 @@
+package com.basics.Strings;
+
+public class StringBldr {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        // Mutable
+
+        sb.toString();
+        // After all alterations are done, convert StringBuilder to String
+    }
+}
