7d6e8faae8f29dffa30727c044c10bd3e8bb996c
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-01-28 18:45:43 +0530

    familiarising with access modifiers

--- src/com/basics/accesmodifier/AccessModifierTest.java
+++ src/com/basics/accesmodifier/AccessModifierTest.java
@@ -1,3 +1,7 @@
+/*
+Working of Access Modifiers
+https://www.javatpoint.com/access-modifiers
+*/
 package com.basics.accesmodifier;
 
 import com.basics.beginnerprograms.AccessModifierInitialising;
@@ -6,17 +10,34 @@ public class AccessModifierTest {
     public static void main(String[] args) {
         AccessModifierInitialising acc = new AccessModifierInitialising();
 //        System.out.println(acc.rollNum);
-        // java: rollNum has private access in
-        // com.basics.beginnerprograms.AccessModifierInitialising
+        // java: rollNum has private access in com.basics.beginnerprograms.AccessModifierInitialising
         /***********************/
         System.out.println(acc.name);
         /***********************/
 //        System.out.println(acc.mailId);
-        // java: mailId has protected access in
-        // com.basics.beginnerprograms.AccessModifierInitialising
+        // java: mailId has protected access in com.basics.beginnerprograms.AccessModifierInitialising
         /***********************/
 //        System.out.println(acc.phoneNum);
-        // java: mailId has protected access in
-        // com.basics.beginnerprograms.AccessModifierInitialising
+        // java: mailId has protected access in com.basics.beginnerprograms.AccessModifierInitialising
+
+        /***********************/
+//        AccessModifierInitialising accarg = new AccessModifierInitialising(2);
+        // AccessModifierInitialising(int) has private access in com.basics.beginnerprograms.AccessModifierInitialising
+
+        Access a1 = new Access();
+        a1.disp();
+        // Accessing protected members using child class
     }
+}
+class Access extends AccessModifierInitialising{
+    public void disp(){
+        System.out.println(mailId); // Accessing protected members using child class
+    }
+    // Method overriding
+//    void printResult(){
+//        System.out.println("Hello World");
+//    }
+    // attempting to assign weaker access privileges; was protected
+    // The default modifier is more restrictive than protected.
+    // That is why, there is a compile-time error.
 }
--- src/com/basics/beginnerprograms/AccessModifierInitialising.java
+++ src/com/basics/beginnerprograms/AccessModifierInitialising.java
@@ -5,4 +5,16 @@ public class AccessModifierInitialising {
     public String name = "Jane";
     protected String mailId = "jane@123.com";
     long phoneNum = 123456789;
+
+    public AccessModifierInitialising() {
+    }
+    private AccessModifierInitialising(int rollNum){
+        this.rollNum = rollNum;
+        name = "Joe";
+        mailId = "joe@123.com";
+        phoneNum = 987654321;
+    }
+    protected void printResult(){
+        System.out.println("Result");
+    }
 }
