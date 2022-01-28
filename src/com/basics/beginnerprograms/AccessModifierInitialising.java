abbd049bbf770996bd14013fb17cc3da2bf02d1c
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-01-28 18:05:29 +0530

    initialising variables for familiarising with access modifiers

---
+++ src/com/basics/beginnerprograms/AccessModifierInitialising.java
@@ -0,0 +1,8 @@
+package com.basics.beginnerprograms;
+
+public class AccessModifierInitialising {
+    private int rollNum = 1;
+    public String name = "Jane";
+    protected String mailId = "jane@123.com";
+    long phoneNum = 123456789;
+}
