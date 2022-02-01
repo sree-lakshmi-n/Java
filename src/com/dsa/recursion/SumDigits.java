62d97b444be3a2dda6bf8e5aeec61e6aaedac21d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-02-01 16:12:11 +0530

    Computes sum of digits using recursion

---
+++ src/com/dsa/recursion/SumDigits.java
@@ -0,0 +1,15 @@
+/* Finds sum of digits of given number using recursion */
+package com.dsa.recursion;
+
+public class SumDigits {
+    public int sumDigits(int n) {
+        if(n==0|| n== 1) return n;
+        return n%10 + sumDigits(n/10);
+    }
+    public static void main(String[] args) {
+        SumDigits sd = new SumDigits();
+        for (int i = 0; i <= 5040; i+=67) {
+            System.out.println("Digit sum of "+ i +" = "+sd.sumDigits(i));
+        }
+    }
+}
