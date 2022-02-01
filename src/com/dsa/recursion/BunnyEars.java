80ba8947b5ab0b74ad06a4fa5ef7a087a3b89624
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-02-01 15:25:26 +0530

    Computes number of bunny ears recursively

---
+++ src/com/dsa/recursion/BunnyEars.java
@@ -0,0 +1,19 @@
+/*Bunny Ears
+* We have a number of bunnies and each bunny has two big floppy ears.
+* We want to compute the total number of ears across all the bunnies recursively.
+* */
+package com.dsa.recursion;
+
+public class BunnyEars {
+    public int bunnyEars(int bunnies) {
+        if(bunnies == 0) return 0;
+        return 2 + bunnyEars(bunnies-1);
+    }
+
+    public static void main(String[] args) {
+        BunnyEars be = new BunnyEars();
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i+" bunnies have "+be.bunnyEars(i)+" ears");
+        }
+    }
+}
