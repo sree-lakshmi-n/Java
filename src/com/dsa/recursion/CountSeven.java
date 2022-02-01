49c68878575d58e58a088283542a0d129903c226
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-02-01 17:37:03 +0530

    count number of 7s in a number using recursion

--- src/com/dsa/recursion/CountSeven.java
+++ src/com/dsa/recursion/CountSeven.java
@@ -1,3 +1,11 @@
+/*
+ * Count7
+ * Given a non-negative int n, return the count of the
+ * occurrences of 7 as a digit, so for example 717 yields 2. (no loops).
+ * Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6),
+ * while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+ * https://codingbat.com/prob/p101409
+ * */
 package com.dsa.recursion;
 
 public class CountSeven {
