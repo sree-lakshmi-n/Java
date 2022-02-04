148098fbdff4bb27aa5d84f5794d9b0e9c4d1f51
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-02-04 15:11:12 +0530

    Finds whether the number 6 is present in an array

--- src/com/dsa/recursion/codingbat/FindSix.java
+++ src/com/dsa/recursion/codingbat/FindSix.java
@@ -1,3 +1,9 @@
+/*
+* Given an array of ints, compute recursively if the array contains a 6.
+* We'll use the convention of considering only the part of the array that
+* begins at the given index. In this way, a recursive call can pass index+1
+* to move down the array. The initial call will pass in index as 0.
+ * */
 package com.dsa.recursion.codingbat;
 
 public class FindSix {
