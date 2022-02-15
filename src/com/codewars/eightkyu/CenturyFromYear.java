282e63bc380b4eeb68a2f83de58da3b1bb3956ff
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-02-15 17:14:57 +0530

    Given a year, returns the century it is in

--- src/com/codewars/eightkyu/CenturyFromYear.java
+++ src/com/codewars/eightkyu/CenturyFromYear.java
@@ -1,2 +1,19 @@
-package com.codewars.eightkyu;public class CenturyFromYear {
+//Century from year
+// Given a year, returns the century it is in.
+// https://www.codewars.com/kata/5a3fe3dde1ce0e8ed6000097/train/java
+
+package com.codewars.eightkyu;
+
+public class CenturyFromYear {
+    public static int century(int number) {
+        return number%100==0?number/100:number/100+1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("1705 -> "+century(1705));
+        System.out.println("1900 -> "+century(1900));
+        System.out.println("1601 -> "+century(1601));
+        System.out.println("2000 -> "+century(2000));
+        System.out.println("89 -> "+century(89));
+    }
 }
