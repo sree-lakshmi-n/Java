e9afd2212ef269bcd0eb173f5eba6b06fd0ea9c0
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-02-15 17:08:16 +0530

    Returns the count of vowels in a given string

---
+++ src/com/codewars/sevenkyu/VowelCount.java
@@ -0,0 +1,28 @@
+// Returns the count of vowels in a given string
+package com.codewars.sevenkyu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class VowelCount {
+    public static int getCount(String str) {
+        List<Character> vowels = new ArrayList<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        return (int)str.toLowerCase().chars().filter(ch -> vowels.contains((char)ch)).count();
+
+        /*
+        * Other methods from codewars:
+        * 1.return str.replaceAll("(?i)[^aeiou]", "").length();
+        * 2.return (int) str.chars().filter(c -> "aeiou".indexOf(c) >= 0).count();
+         *3. return str.replaceAll("[^aeiou]","").length();
+         * 4. (int)str.chars().mapToObj(i -> (char)i).filter(i -> "aeiou".contains(String.valueOf(i))).count();
+        * */
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getCount("aeiou"));
+        System.out.println(getCount("abracadabra"));
+        System.out.println(getCount("Nope"));
+        System.out.println(getCount(""));
+    }
+}
