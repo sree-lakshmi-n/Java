e2b5c4d67d97d771f5ee52a9f110ce2d2eb6b443
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-09-28 10:51:20 +0530

    Defined main class and called evenOrOdd()

--- src/com/basics/beginnerprograms/BasicOperations.java
+++ src/com/basics/beginnerprograms/BasicOperations.java
@@ -1,7 +1,17 @@
 package com.basics.beginnerprograms;
 
+import java.util.Scanner;
+
 public class BasicOperations {
     private String evenOrOdd(int num){
         return num%2==0?"even":"odd";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BasicOperations bo = new BasicOperations();
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println(bo.evenOrOdd(num));
     }
 }
