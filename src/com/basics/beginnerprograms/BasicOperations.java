9643d03d865b905ff43da57a74091b924d3cd3eb
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-09-28 11:00:59 +0530

    called CelsiusToFahrenheit()

--- src/com/basics/beginnerprograms/BasicOperations.java
+++ src/com/basics/beginnerprograms/BasicOperations.java
@@ -8,15 +8,20 @@ public class BasicOperations {
         return num%2==0?"even":"odd";
     }
     // to convert the temperature in Degree Centigrade to Fahrenheit
-    private int CelsiusToFarenheit(int celsius){
-        return celsius*(9/5)+32;
+    private float CelsiusToFahrenheit(float celsius){
+        return (celsius*9/5)+32;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         BasicOperations bo = new BasicOperations();
+        System.out.println("********* Even/Odd ***************");
         System.out.println("Enter a number: ");
         int num = sc.nextInt();
         System.out.println(bo.evenOrOdd(num));
+        System.out.println("********* Celsius to Fahrenheit Converter ***************");
+        System.out.println("Enter temperature in °C");
+        float celsius = sc.nextFloat();
+        System.out.println(celsius+"°C = "+bo.CelsiusToFahrenheit(celsius)+"F");
     }
 }
