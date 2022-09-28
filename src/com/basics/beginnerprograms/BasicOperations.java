f68ff911bdea6eb21b6ff1773a5c32cae08f6d43
sree-lakshmi-n <sreelakshmin153@gmai.com>
2022-09-28 10:47:03 +0530

    evenOrOdd class

--- src/com/basics/beginnerprograms/BasicOperations.java
+++ src/com/basics/beginnerprograms/BasicOperations.java
@@ -1,2 +1,7 @@
-package com.basics.beginnerprograms;public class BasicOperations {
+package com.basics.beginnerprograms;
+
+public class BasicOperations {
+    private String evenOrOdd(int num){
+        return num%2==0?"even":"odd";
+    }
 }
